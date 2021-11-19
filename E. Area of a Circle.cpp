5f4d8e9ce67ef0f3e811330d917926cfed96367a
joey-turnerr <93777270+joey-turnerr@users.noreply.github.com>
2021-11-19 22:53:21 +0000

    Create E. Area of a Circle.cpp

---
+++ E. Area of a Circle.cpp
@@ -0,0 +1,12 @@
+    #include <iostream>
+    #include <iomanip>
+    using namespace std;
+    int main ()
+    {
+        float r;
+        double pi =3.141592653;
+        cin >>r;
+        cout<< fixed <<setprecision(9)<<(pi*r*r);
+        return 0;
+     
+    }
