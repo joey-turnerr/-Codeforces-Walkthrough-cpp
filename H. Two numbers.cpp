e3aa67f159b2aa9e92a418ac47f52d168ce2f3f6
joey-turnerr <93777270+joey-turnerr@users.noreply.github.com>
2021-11-19 23:30:18 +0000

    Create H. Two numbers.cpp

---
+++ H. Two numbers.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>    //use <cmath> if you are including iostream
+using namespace std;
+
+int main()
+{
+    float a,b ;
+    cin >>a>>b;
+    cout << "floor "<<a<<" / "<<b<<" = "<<floor(a/b)<<"\n";
+    cout << "ceil "<<a<<" / "<<b<<" = "<<ceil(a/b)<<"\n";
+    cout << "round "<<a<<" / "<<b<<" = "<<round(a/b)<<"\n";
+
+    return 0;
+}
