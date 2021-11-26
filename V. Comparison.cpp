f7be09d25967fa5058a2ad9ad6bd7155dfd2585d
joey-turnerr <93777270+joey-turnerr@users.noreply.github.com>
2021-11-26 16:29:40 +0000

    Create V. Comparison.cpp

---
+++ V. Comparison.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    int a,b;
+    char s;
+    cin>>a>>s>>b;
+
+    switch (s)
+    {
+        case '>':
+            if (a>b)
+            {
+                cout<<"Right";
+                break;
+            }
+            else
+            {
+                cout<<"Wrong";
+                break;
+            }
+
+        case '=':
+            if (a==b)
+            {
+                cout<<"Right";
+                break;
+            }
+            else
+            {
+                cout<<"Wrong";
+                break;
+            }
+
+        case '<':
+            if (a<b)
+            {
+                cout<<"Right";
+                break;
+            }
+            else
+            {
+                cout<<"Wrong";
+                break;
+            }
+
+    }
+    return 0;
+}
