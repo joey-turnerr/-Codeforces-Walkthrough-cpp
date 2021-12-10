4691b391682263272febd5d617d1cb6141962761
joey-turnerr <93777270+joey-turnerr@users.noreply.github.com>
2021-12-10 12:27:23 +0000

    Create C. Next Alphabet.cpp

---
+++ C. Next Alphabet.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    char x;
+    cin>>x;
+    int z = x;
+    if( z == 122)
+        cout<<"a";
+    else
+        cout<<char(x+1);
+
+
+    return 0;
+}
